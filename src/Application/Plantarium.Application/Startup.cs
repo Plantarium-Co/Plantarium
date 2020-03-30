fdca7d5c5d5970e43b5ebc02f7506e3679afc4d0
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2020-03-30 19:32:10 +0800

    Added identity context and migrations.

---
+++ src/Application/Plantarium.Application/Startup.cs
@@ -0,0 +1,81 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Startup.cs" company="Plantarium Co.">
+//     Plantarium, All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Plantarium.Application
+{
+    using System;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Plantarium.Data.Contexts;
+
+    /// <summary>
+    /// The startup class used for configuring the services.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        /// <value>
+        /// The configuration.
+        /// </value>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Configures the services.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<IdentityDbContext>(options =>
+                options.UseSqlServer(
+                    this.Configuration.GetConnectionString("Default"), 
+                    assemblyOptions => assemblyOptions.MigrationsAssembly("Plantarium.Data")));
+
+            services.AddDefaultIdentity<IdentityUser<Guid>>()
+                .AddEntityFrameworkStores<IdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        /// <summary>
+        /// Configures the specified application.
+        /// </summary>
+        /// <param name="app">The application.</param>
+        /// <param name="env">The env.</param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ src/Infrastructure/Plantarium.Data/Contexts/IdentityDbContext.cs
@@ -0,0 +1,53 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IdentityDbContext.cs" company="Plantarium Co.">
+//     Plantarium, All rights reserved
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Plantarium.Data.Contexts
+{
+    using System;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using External = Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+    /// <summary>
+    /// The identity database context.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityDbContext{Microsoft.AspNetCore.Identity.IdentityUser{System.Guid}, Microsoft.AspNetCore.Identity.IdentityRole{System.Guid}, System.Guid}" />
+    public class IdentityDbContext : External.IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentityDbContext"/> class.
+        /// </summary>
+        /// <param name="options">The options.</param>
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Configures the schema needed for the identity framework.
+        /// </summary>
+        /// <param name="builder">The builder being used to construct the model for this context.</param>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Identity schema
+            builder.HasDefaultSchema("identity");
+
+            // Identity tables
+            builder.Entity<IdentityUser<Guid>>(entity => entity.ToTable("Users"));
+            builder.Entity<IdentityRole<Guid>>(entity => entity.ToTable("Roles"));
+            builder.Entity<IdentityUserRole<Guid>>(entity => entity.ToTable("UserRoles"));
+            builder.Entity<IdentityUserClaim<Guid>>(entity => entity.ToTable("UserClaims"));
+            builder.Entity<IdentityRoleClaim<Guid>>(entity => entity.ToTable("RoleClaims"));
+            builder.Entity<IdentityUserLogin<Guid>>(entity => entity.ToTable("UserLogins"));
+            builder.Entity<IdentityUserToken<Guid>>(entity => entity.ToTable("UserTokens"));
+
+            // Identity Indexes
+            builder.Entity<IdentityUser<Guid>>().HasIndex(u => u.NormalizedUserName).HasName("IX_Users_NormalizedUserName").IsUnique();
+            builder.Entity<IdentityUser<Guid>>().HasIndex(u => u.NormalizedEmail).HasName("IX_Users_NormalizedEmail");
+            builder.Entity<IdentityRole<Guid>>().HasIndex(r => r.NormalizedName).HasName("IX_Roles_NormalizedName").IsUnique();
+        }
+    }
+}
