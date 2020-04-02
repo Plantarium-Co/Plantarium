e4f332641463cc5cb8789cf21184678827bdbb44
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2020-04-02 22:24:15 +0800

    Added token providers and wrapped identity logic.

---
+++ src/Infrastructure/Plantarium.Infrastructure/Providers/Interfaces/ITokenProvider.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AuthException.cs" company="Plantarium Co.">
+//     Plantarium, MIT
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Plantarium.Infrastructure.Providers.Interfaces
+{
+    using System.Collections.Generic;
+    using System.Security.Claims;
+
+    /// <summary>
+    /// The token provider.
+    /// </summary>
+    public interface ITokenProvider
+    {
+        /// <summary>
+        /// Generates the token.
+        /// </summary>
+        /// <param name="claims">The claims.</param>
+        /// <returns>The generated token.</returns>
+        string GenerateToken(IEnumerable<Claim> claims);
+    }
+}
---
+++ src/Infrastructure/Plantarium.Infrastructure/Providers/TokenProvider.cs
@@ -0,0 +1,63 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AuthException.cs" company="Plantarium Co.">
+//     Plantarium, MIT
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Plantarium.Infrastructure.Providers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+    using System.Text;
+    using Microsoft.IdentityModel.Tokens;
+    using Plantarium.Infrastructure.Providers.Interfaces;
+
+    /// <summary>
+    /// The token provider.
+    /// </summary>
+    /// <seealso cref="Plantarium.Infrastructure.Providers.Interfaces.ITokenProvider" />
+    public class TokenProvider : ITokenProvider
+    {
+        /// <summary>
+        /// The secret.
+        /// </summary>
+        private const string SECRET = "Plantarium";
+
+        /// <summary>
+        /// The expiry in days.
+        /// </summary>
+        private const double EXPIRY_DAYS = 7;
+
+        /// <summary>
+        /// The algorithm.
+        /// </summary>
+        private const string ALGORITHM = SecurityAlgorithms.HmacSha256Signature;
+
+        /// <summary>
+        /// Generates the token.
+        /// </summary>
+        /// <param name="claims">The claims.</param>
+        /// <returns>
+        /// The generated token.
+        /// </returns>
+        public string GenerateToken(IEnumerable<Claim> claims)
+        {
+            var result = string.Empty;
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(SECRET);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(EXPIRY_DAYS),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), ALGORITHM)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            result = tokenHandler.WriteToken(token);
+
+            return result;
+        }
+    }
+}
