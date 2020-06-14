f8ee42bef3d53d35efa2122db95cb81ef8f4c3b8
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2020-06-14 18:34:01 +0800

    Added Sql Helper for database communication.

---
+++ src/Infrastructure/Plantarium.Infrastructure/Helpers/ISqlHelper.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ISqlHelper.cs" company="Plantarium Co.">
+//     Plantarium, MIT
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Plantarium.Infrastructure.Helpers
+{
+    using System.Collections.Generic;
+    using System.Data.Common;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The SQL helper.
+    /// </summary>
+    public interface ISqlHelper
+    {
+        /// <summary>
+        /// Executes the command asynchronous.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <returns>The task.</returns>
+        Task ExecuteAsync(DbCommand command);
+
+        /// <summary>
+        /// Queries the result asynchronous.
+        /// </summary>
+        /// <typeparam name="T">The object type.</typeparam>
+        /// <param name="command">The command.</param>
+        /// <returns>The list of query result.</returns>
+        Task<IEnumerable<T>> QueryAsync<T>(DbCommand command) where T : class, new();
+
+        /// <summary>
+        /// Queries the first result asynchronous.
+        /// </summary>
+        /// <typeparam name="T">The object type.</typeparam>
+        /// <param name="command">The command.</param>
+        /// <returns>The first query result.</returns>
+        Task<T> QueryFirstAsync<T>(DbCommand command);
+    }
+}
