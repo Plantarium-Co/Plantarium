ccab8ce9a5dbd5fa6e0e932fd18107af21a57f6e
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2020-06-20 23:58:33 +0800

    Updated reflection optimizer implementation and seperated reflection utilities.

--- src/Infrastructure/Plantarium.Infrastructure/Internals/ReflectionOptimizer.cs
+++ src/Infrastructure/Plantarium.Infrastructure/Internals/ReflectionOptimizer.cs
@@ -6,7 +6,6 @@
 namespace Plantarium.Infrastructure.Internals
 {
     using System;
-    using System.Collections.Generic;
     using System.Reflection;
 
     /// <summary>
@@ -15,54 +14,45 @@ namespace Plantarium.Infrastructure.Internals
     internal static class ReflectionOptimizer
     {
         /// <summary>
-        /// Caches the property setters.
+        /// Creates the weakly typed object getter delegate.
         /// </summary>
-        /// <typeparam name="T">The object type.</typeparam>
-        /// <param name="properties">The properties.</param>
-        /// <returns>The object property setters cache.</returns>
-        public static Dictionary<string, Action<T, object>> CachePropertySetters<T>(IEnumerable<string> properties) where T : class, new()
+        /// <param name="method">The method.</param>
+        /// <returns>The object getter delegate.</returns>
+        public static Func<object, object> CreateObjectGetterDelegate(MethodInfo method)
         {
-            var propertySettersCache = new Dictionary<string, Action<T, object>>();
-
-            foreach (var property in properties)
-            {
-                var methodInfo = typeof(T).GetProperty(property)?.GetSetMethod();
-
-                if (methodInfo != null)
-                {
-                    var setter = CreateObjectSetterDelegate<T>(methodInfo);
-                    propertySettersCache.Add(property, setter);
-                }
-            }
-
-            return propertySettersCache;
+            return (model) => method.Invoke(model, null);
         }
 
         /// <summary>
-        /// Creates the object setter delegate.
+        /// Creates the strongly typed object getter delegate.
+        /// </summary>
+        /// <typeparam name="T">The object type.</typeparam>
+        /// <param name="method">The method.</param>
+        /// <returns>The object getter delegate.</returns>
+        public static Func<T, object> CreateObjectGetterDelegate<T>(MethodInfo method)
+        {
+            return (Func<T, object>)(object)CreateObjectGetterDelegate(method);
+        }
+
+        /// <summary>
+        /// Creates the weakly typed object setter delegate.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        /// <returns>The object setter delegate.</returns>
+        public static Action<object, object> CreateObjectSetterDelegate(MethodInfo method)
+        {
+            return (model, value) => method.Invoke(model, new object[] { value });
+        }
+
+        /// <summary>
+        /// Creates the strongly typed object setter delegate.
         /// </summary>
         /// <typeparam name="T">The object type.</typeparam>
         /// <param name="method">The method.</param>
         /// <returns>The object setter delegate.</returns>
         public static Action<T, object> CreateObjectSetterDelegate<T>(MethodInfo method)
         {
-            MethodInfo openImpl = typeof(ReflectionOptimizer).GetMethod(nameof(CreateObjectSetterDelegateImpl));
-            MethodInfo closedImpl = openImpl.MakeGenericMethod(typeof(T), method.GetParameters()[0].ParameterType);
-            return (Action<T, object>)closedImpl.Invoke(null, new object[] { method });
-        }
-
-        /// <summary>
-        /// Creates the object setter delegate implementation.
-        /// </summary>
-        /// <typeparam name="TSource">The type of the source.</typeparam>
-        /// <typeparam name="TParam">The type of the parameter.</typeparam>
-        /// <param name="method">The method.</param>
-        /// <returns>The casted object setter delegate.</returns>
-        public static Action<TSource, object> CreateObjectSetterDelegateImpl<TSource, TParam>(MethodInfo method)
-        {
-            object tdelegate = Delegate.CreateDelegate(typeof(Action<TSource, TParam>), null, method);
-            Action<TSource, TParam> call = (Action<TSource, TParam>)tdelegate;
-            return delegate(TSource source, object parameter) { call(source, (TParam)parameter); };
+            return (Action<T, object>)(object)CreateObjectSetterDelegate(method);
         }
     }
 }
---
+++ src/Infrastructure/Plantarium.Infrastructure/Internals/ReflectionUtilities.cs
@@ -0,0 +1,176 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ReflectionUtilities.cs" company="Plantarium Co.">
+//     Plantarium, MIT
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Plantarium.Infrastructure.Internals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using static Plantarium.Infrastructure.Internals.ReflectionOptimizer;
+
+    /// <summary>
+    /// The reflection utilities.
+    /// </summary>
+    internal static class ReflectionUtilities
+    {
+        /// <summary>
+        /// The default binding flags.
+        /// </summary>
+        private static readonly BindingFlags DefaultFlags = BindingFlags.Public | BindingFlags.Instance;
+
+        /// <summary>
+        /// The ignore case binding flags.
+        /// </summary>
+        private static readonly BindingFlags IgnoreCaseFlags = DefaultFlags | BindingFlags.IgnoreCase;
+
+        /// <summary>
+        /// Caches the strongly typed property getters.
+        /// </summary>
+        /// <typeparam name="T">The object type.</typeparam>
+        /// <returns>The object property getters cache.</returns>
+        public static Dictionary<string, Func<T, object>> CachePropertyGetters<T>()
+        {
+            var propertyGettersCache = new Dictionary<string, Func<T, object>>();
+            var properties = typeof(T).GetProperties();
+
+            foreach (var property in properties)
+            {
+                var methodInfo = property?.GetGetMethod();
+
+                if (methodInfo != null)
+                {
+                    var getter = CreateObjectGetterDelegate<T>(methodInfo);
+                    propertyGettersCache.Add(property.Name, getter);
+                }
+            }
+
+            return propertyGettersCache;
+        }
+
+        /// <summary>
+        /// Caches the weakly typed property getters.
+        /// </summary>
+        /// <param name="model">The object model.</param>
+        /// <returns>The object property getters cache.</returns>
+        public static Dictionary<string, Func<object, object>> CachePropertyGetters(object model)
+        {
+            var propertyGettersCache = new Dictionary<string, Func<object, object>>();
+            var properties = model.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                var methodInfo = property?.GetGetMethod();
+
+                if (methodInfo != null)
+                {
+                    var getter = CreateObjectGetterDelegate(methodInfo);
+                    propertyGettersCache.Add(property.Name, getter);
+                }
+            }
+
+            return propertyGettersCache;
+        }
+
+        /// <summary>
+        /// Caches the strongly typed property getters against a list of required property names.
+        /// </summary>
+        /// <typeparam name="T">The object type.</typeparam>
+        /// <param name="propertyNames">The property names.</param>
+        /// <param name="ignoreCase">if set to <c>true</c> [ignore case].</param>
+        /// <returns>The object property getters cache.</returns>
+        public static Dictionary<string, Func<T, object>> CachePropertyGetters<T>(IEnumerable<string> propertyNames, bool ignoreCase = false)
+        {
+            var propertyGettersCache = new Dictionary<string, Func<T, object>>();
+            var flags = ignoreCase ? IgnoreCaseFlags : DefaultFlags;
+
+            foreach (var propertyName in propertyNames)
+            {
+                var methodInfo = typeof(T).GetProperty(propertyName, flags)?.GetGetMethod();
+
+                if (methodInfo != null)
+                {
+                    var getter = CreateObjectGetterDelegate<T>(methodInfo);
+                    propertyGettersCache.Add(propertyName, getter);
+                }
+            }
+
+            return propertyGettersCache;
+        }
+
+        /// <summary>
+        /// Caches the strongly typed property setters.
+        /// </summary>
+        /// <typeparam name="T">The object type.</typeparam>
+        /// <returns>The object property setters cache.</returns>
+        public static Dictionary<string, Action<T, object>> CachePropertySetters<T>()
+        {
+            var propertySettersCache = new Dictionary<string, Action<T, object>>();
+            var properties = typeof(T).GetProperties();
+
+            foreach (var property in properties)
+            {
+                var methodInfo = property?.GetSetMethod();
+
+                if (methodInfo != null)
+                {
+                    var setter = CreateObjectSetterDelegate<T>(methodInfo);
+                    propertySettersCache.Add(property.Name, setter);
+                }
+            }
+
+            return propertySettersCache;
+        }
+
+        /// <summary>
+        /// Caches the weakly typed property setters.
+        /// </summary>
+        /// <param name="model">The object model.</param>
+        /// <returns>The object property setters cache.</returns>
+        public static Dictionary<string, Action<object, object>> CachePropertySetters(object model)
+        {
+            var propertySettersCache = new Dictionary<string, Action<object, object>>();
+            var properties = model.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                var methodInfo = property?.GetSetMethod();
+
+                if (methodInfo != null)
+                {
+                    var setter = CreateObjectSetterDelegate(methodInfo);
+                    propertySettersCache.Add(property.Name, setter);
+                }
+            }
+
+            return propertySettersCache;
+        }
+
+        /// <summary>
+        /// Caches the strongly typed property setters against a list of required property names.
+        /// </summary>
+        /// <typeparam name="T">The object type.</typeparam>
+        /// <param name="propertyNames">The property names.</param>
+        /// <param name="ignoreCase">if set to <c>true</c> [ignore case].</param>
+        /// <returns>The object property setters cache.</returns>
+        public static Dictionary<string, Action<T, object>> CachePropertySetters<T>(IEnumerable<string> propertyNames, bool ignoreCase = false)
+        {
+            var propertySettersCache = new Dictionary<string, Action<T, object>>();
+            var flags = ignoreCase ? IgnoreCaseFlags : DefaultFlags;
+
+            foreach (var propertyName in propertyNames)
+            {
+                var methodInfo = typeof(T).GetProperty(propertyName, flags)?.GetSetMethod();
+
+                if (methodInfo != null)
+                {
+                    var setter = CreateObjectSetterDelegate<T>(methodInfo);
+                    propertySettersCache.Add(propertyName, setter);
+                }
+            }
+
+            return propertySettersCache;
+        }
+    }
+}
