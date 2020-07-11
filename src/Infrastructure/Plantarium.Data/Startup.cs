02eda152166a9e2b06209aa365eac73778e83920
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2020-07-11 21:54:03 +0800

    Hooked up the projects using their respective Startup files

---
+++ src/Infrastructure/Plantarium.Data/Startup.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Startup.cs" company="Plantarium Co.">
+//     Plantarium, MIT
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Plantarium.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    /// The data startup.
+    /// </summary>
+    public static class Startup
+    {
+        /// <summary>
+        /// Adds the data.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <param name="configuration">The configuration.</param>
+        public static void AddData(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IdentityDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("Database"),
+                    assemblyOptions => assemblyOptions.MigrationsAssembly("Plantarium.Data")));
+        }
+    }
+}
