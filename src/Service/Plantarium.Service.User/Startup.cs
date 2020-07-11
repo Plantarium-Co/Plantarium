510d5997622a5cbae28df736e290a840b34890d7
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2020-07-11 21:54:03 +0800

    Hooked up the projects using their respective Startup files

---
+++ src/Service/Plantarium.Service.User/Startup.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Startup.cs" company="Plantarium Co.">
+//     Plantarium, MIT
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Plantarium.Service.User
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Plantarium.Service.User.Repositories;
+    using Plantarium.Service.User.Repositories.Interfaces;
+
+    /// <summary>
+    /// The user service startup.
+    /// </summary>
+    public static class Startup
+    {
+        /// <summary>
+        /// Adds the user service.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        public static void AddUserService(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserService, UserService>();
+        }
+    }
+}
