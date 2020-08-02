0114218b5a2f800c7090b446d19e47d52b77e8b9
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2020-08-02 23:31:22 +0800

    Added request validation

---
+++ src/Service/Plantarium.Service.User/Models/Login/LoginRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="LoginRequestValidator.cs" company="Plantarium Co.">
+//     Plantarium, MIT
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Plantarium.Service.User.Models.Login
+{
+    using FluentValidation;
+
+    /// <summary>
+    /// The login request validator.
+    /// </summary>
+    /// <seealso cref="FluentValidation.AbstractValidator{Plantarium.Service.User.Models.Login.LoginRequest}" />
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoginRequestValidator"/> class.
+        /// </summary>
+        public LoginRequestValidator()
+        {
+            this.RuleFor(request => request.Username).NotEmpty();
+            this.RuleFor(request => request.Password).NotEmpty();
+        }
+    }
+}
