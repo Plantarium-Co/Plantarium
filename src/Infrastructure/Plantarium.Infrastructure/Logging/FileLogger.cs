ae468c94feaf16a075e59a53fbc690de83ed353a
Gabriel Ian Escober Abel <gabrielabel_69@yahoo.com>
2020-08-08 19:27:28 +0800

    Implemented file logging

---
+++ src/Infrastructure/Plantarium.Infrastructure/Logging/FileLogger.cs
@@ -0,0 +1,60 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="FileLogger.cs" company="Plantarium Co.">
+//     Plantarium, MIT
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Plantarium.Infrastructure.Logging
+{
+    using Microsoft.Extensions.Configuration;
+    using Serilog;
+    using Internal = Interfaces;
+
+    /// <summary>
+    /// The file logger.
+    /// </summary>
+    public class FileLogger : Internal.ILogger
+    {
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        private readonly ILogger logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileLogger"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        public FileLogger(IConfiguration configuration)
+        {
+            this.logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration, "FileLogging")
+                .CreateLogger();
+        }
+
+        /// <summary>
+        /// Logs an information.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public void Information(string message)
+        {
+            this.logger.Information(message);
+        }
+
+        /// <summary>
+        /// Logs a warning.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public void Warning(string message)
+        {
+            this.logger.Warning(message);
+        }
+
+        /// <summary>
+        /// Logs an error.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public void Error(string message)
+        {
+            this.logger.Error(message);
+        }
+    }
+}
---
+++ src/Infrastructure/Plantarium.Infrastructure/Logging/Interfaces/ILogger.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ILogger.cs" company="Plantarium Co.">
+//     Plantarium, MIT
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Plantarium.Infrastructure.Logging.Interfaces
+{
+    /// <summary>
+    /// The logger.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Logs an error.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        void Error(string message);
+
+        /// <summary>
+        /// Logs an information.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        void Information(string message);
+
+        /// <summary>
+        /// Logs a warning.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        void Warning(string message);
+    }
+}
